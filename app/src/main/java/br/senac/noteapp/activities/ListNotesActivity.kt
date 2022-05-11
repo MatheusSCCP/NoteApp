e013e759368ae8297b7df75ebc998fe7d9d4b939
MatheusSCCP <matheus-souza10@live.com>
2022-05-10 22:09:56 -0300

    Room.

--- app/src/main/java/br/senac/noteapp/activities/ListNotesActivity.kt
+++ app/src/main/java/br/senac/noteapp/activities/ListNotesActivity.kt
@@ -3,6 +3,9 @@ package br.senac.noteapp.activities
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.room.Room
+import br.senac.noteapp.data.AppDatabase
+import br.senac.noteapp.data.Note
 import br.senac.noteapp.data.NoteObject
 import br.senac.noteapp.databinding.ActivityListNotesBinding
 import br.senac.noteapp.databinding.CardNoteBinding
@@ -24,13 +27,23 @@ class ListNotesActivity : AppCompatActivity() {
 
     fun OnResume() {
         super.onResume()
-        updateNotes()
+        updatesNotes()
+    }
+    fun updatesNotes(){
+        Thread{
+            val db = Room.databaseBuilder(this, AppDatabase::class.java, "db").build()
+            val list = db.noteDao().listAll()
+            //Roda o codigo da tela
+            runOnUiThread {
+                updateUI(list)
+            }
+        }.start()
     }
 
-    fun updateNotes() {
+    fun updateUI(note: List<Note>) {
         binding.container.removeAllViews()
-
-        NoteObject.listNotes.forEach {
+           //Atualiza a tela
+            note.forEach {
             val card = CardNoteBinding.inflate(layoutInflater)
 
             card.textTitle.text = it.title
